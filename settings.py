f62080d41b6e1b7b6fdf30070177de2f77f5faa4
Brian <bt@Hg.local>
2012-02-24 21:07:00 -0500

    Added templates and CSS

--- settings.py
+++ settings.py
@@ -56,7 +56,7 @@ MEDIA_URL = 'userfiles/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'C:/Users/Oliver/Notesharing/conga/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
