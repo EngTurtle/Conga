cda47ac59a38282192cacff5f6c30b0bbc392f5b
EngTurtle <ollie.liang@gmail.com>
2012-03-07 19:40:09 -0500

    Added the urls for the registering page

--- urls.py
+++ urls.py
@@ -6,18 +6,21 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'engsci_conga.views.home', name="root"),
+                       url(r'^$', 'engsci_conga.views.home', name = "root"),
 
-    # Examples:
-    # url(r'^$', 'testing.views.home', name='home'),
-    # url(r'^testing/', include('testing.foo.urls')),
+                       # Examples:
+                       # url(r'^$', 'testing.views.home', name='home'),
+                       # url(r'^testing/', include('testing.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       # Uncomment the admin/doc line below to enable admin documentation:
+                       url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+                       # Uncomment the next line to enable the admin:
+                       url(r'^admin/', include(admin.site.urls)),
 
-    # Conga
-    url(r'^course/', include('engsci_conga.urls')),
+                       # Conga
+                       url(r'^course/', include('engsci_conga.urls')),
+
+                       # User management
+                       url(r'^register/', include('user_manage.urls'))
 )
--- user_manage/urls.py
+++ user_manage/urls.py
@@ -3,5 +3,5 @@ __author__ = 'Oliver'
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('user_manage.views',
-                       url(r'^register/$', 'register'),
+                       url(r'^$', 'register'),
                        )
