9864ca09b9e69c2c7c6503f0d368e69067628308
Brian <bt@hg.mshome.net>
2012-03-31 21:32:32 -0400

    Made changes on json (still not working)

--- engsci_conga/urls.py
+++ engsci_conga/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('engsci_conga.views',
                        url(r'^(?P<course>\w{3}\d{3,4})/$', 'courses_view'),
-
+                        url(r'all.json', 'list_all_courses')
                        )
