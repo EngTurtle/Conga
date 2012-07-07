4fd85e6e4e423d7af2d6020d27585f82c6e06714
Oliver Liang <ollie.liang@gmail.com>
2012-07-07 18:40:25 -0300

    Revert "SQLite uses the .db extension"
    This reverts commit 61b9c99278001fa3394304e205ad455221df3097.
    
    fully reverting to may 2 code

--- Doc_sharing/urls.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'Oliver'
-
-from django.conf.urls.defaults import patterns, url
-
-urlpatterns = patterns('Doc_sharing.views',
-                       url(r'^(?P<user>\w*)/(?P<pk>\d*)/$', 'file_download'),
-                       url(r'^(?P<user>\w*)/(?P<pk>\d*)/delete/$', 'doc_delete'),
-                       )
